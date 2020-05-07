e0197d24926f3896c6ed5393415d91d3ce9b3194
M4RKERGit <62885141+M4RKERGit@users.noreply.github.com>
2020-05-07 15:33:07 +0300

    Add files via upload

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -1,13 +1,22 @@
+`timescale 1ns / 1ps
+
 module counter(
-    input clk100_i,
-    input rstn_i,
-    input [9:0] sw_i,
-    input [1:0] key_i,
+    input        clk100_i,
+    input        rstn_i,
+    input  [9:0] sw_i,
+    input  [1:0] key_i,
     output [9:0] ledr_o,
     output [6:0] hex1_o,
     output [6:0] hex0_o
-);
-
-
-
+    );
+  
+reg_to_ledr secmod
+( .sw_i(sw_i),
+  .ledr_o(ledr_o));
+  
+ccccounter thirdmod
+( .key_i(key_i),
+  .hex1_o(hex1_o),
+  .hex0_o(hex0_o));
+ 
 endmodule

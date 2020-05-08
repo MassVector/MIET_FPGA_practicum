6310d5c8343d9cb99107188025841fe4ffb025e4
alexpa940 <alexpa940@gmail.com>
2020-05-08 23:12:34 +0300

    try to fix lab2

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -7,30 +7,47 @@ module counter(
     input  [1:0] key_i,
     output [9:0] ledr_o,
     output [6:0] hex1_o,
-    output [6:0] hex0_o
-    );
+    output [6:0] hex0_o);
+
 reg  [7:0] counter = 8'd0 ;
 reg  [9:0] q       = 10'b0;
 
-always @ (posedge clk100_i) begin				
-    if  ( key_i[1] ) begin
+wire [1:0] bt_down;
+
+button_debounce bt0(
+  .btn_i   ( !key_i[0]  ),
+  .ondn_o  ( bt_down[0] ),
+  .clk_i   ( clk100_i   )
+); 
+button_debounce bt1(
+  .btn_i   ( !key_i[1]  ),
+  .ondn_o  ( bt_down[1] ),
+  .clk_i   ( clk100_i   )
+);
+
+always @( posedge clk100_i or posedge bt_down[1] )
+  begin
+    if  ( bt_down[1] ) begin
         q = 10'b0;
         counter = 8'd0;
     end
-    else if ( key_i[0] ) begin
-            q = sw_i;
-            counter = counter +1;
-         end
-end
+    else
+      if ( bt_down[0] ) begin
+         q       = sw_i;
+         counter = counter +1;
+      end
+  end
 
 assign ledr_o = q;
 
-dec_hex dec0
-( .in(counter[7:4]),
- .OUT(hex0_o));
- 
-dec_hex dec1
-( .in(counter[3:0]),
-  .OUT(hex1_o));
- 
+dec_hex dec0(
+  .in ( counter[7:4] ),
+  .out( hex0_o       )
+);
+
+dec_hex dec1(
+  .in ( counter[3:0] ),
+  .out( hex1_o       )
+);
+
 endmodule
--- lab2/tb/counter_tb.v
+++ lab2/tb/counter_tb.v
@@ -2,8 +2,7 @@
 
 
 module counter_tb(
-
-    );
+);
 localparam CLK_FREQ_MHZ   = 50;
 localparam CLK_SEMIPERIOD = ( 1000 / CLK_FREQ_MHZ) / 2;     
 
@@ -15,32 +14,43 @@ wire [6:0]   hex1_o;
 wire [6:0]   hex0_o;
   
 counter DUT (
- .sw_i     (sw_i    ),
- .ledr_o   (ledr_o  ),
- .clk100_i (clk100_i),
- .key_i    (key_i   ),
- .hex1_o   (hex1_o  ),
- .hex0_o   (hex0_o  )
+  .sw_i     ( sw_i    ),
+  .ledr_o   ( ledr_o  ),
+  .clk100_i ( clk100_i),
+  .key_i    ( key_i   ),
+  .hex1_o   ( hex1_o  ),
+  .hex0_o   ( hex0_o  )
 );
+reg [1:0] sw = 2'b11;
 
-assign key_i[1] = 0;
 //assign key_i[1] = 1;
-assign key_i[0] = 1;
+//assign key_i[0] = 1;
 //assign key_i[0] = $random();
 
 initial begin
-    clk100_i = 1'b1;
-    forever begin
-        #CLK_SEMIPERIOD clk100_i=~clk100_i;
-    end
+  clk100_i = 1'b1;
+  forever begin
+            #CLK_SEMIPERIOD clk100_i=~clk100_i;
+          end
 end
 
 initial begin
-    sw_i[9:0] = 10'd0;
-    repeat(16)begin
-        #(CLK_SEMIPERIOD - 1);
-        sw_i[9:0] = $random();
-    end
-end
+  sw_i[9:0] = 10'd1;
+  //repeat(16)begin
+  forever begin
+            #(CLK_SEMIPERIOD - 1);
+            sw_i[9:0] = $random();
+          end
+        end
+
+initial begin
+          repeat(16)
+          begin
+            #50;
+            sw = $random();
+           end
+         end
+
+assign key_i = sw;
 
 endmodule

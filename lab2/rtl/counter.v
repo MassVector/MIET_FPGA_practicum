f8ca3291e0b718ee35a9cb1789ff0894fc7492d3
somenewacc <62895232+somenewacc@users.noreply.github.com>
2020-05-09 22:14:28 +0300

    lab2 fix

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -1,36 +1,46 @@
 `timescale 1ns / 1ps
-module counter(
-    input        clk100_i,
-    input        rstn_i,
-    input  [9:0] sw_i,
-    input  [1:0] key_i,
-    output [9:0] ledr_o,
-    output [6:0] hex1_o,
-    output [6:0] hex0_o
-    );
-    
-reg  [7:0] counter = 8'd0;
-reg  [9:0] q       = 10'b0;
 
-always @(posedge clk100_i) begin
-    if  (key_i[1]) begin
-        q <= 10'b0;
-        counter <= 8'd0;
+module counter(
+  input        clk100_i,
+  input        rstn_i,
+  input  [9:0] sw_i,
+  input  [1:0] key_i,
+  output [9:0] ledr_o,
+  output [6:0] hex1_o,
+  output [6:0] hex0_o);
+    
+reg  [7:0] counter;
+reg  [9:0] q;
+
+wire bt_down;
+
+key_debouncer bt(
+  .btn_i   ( !key_i[0] ),
+  .rst_i   ( key_i[1] ),
+  .ondn_o  ( bt_down  ),
+  .clk_i   ( clk100_i )
+);
+
+always @( posedge clk100_i or negedge key_i[1] ) begin
+  if  ( !key_i[1] ) begin
+    q       <= 10'b0;
+    counter <= 8'd0;
+  end
+  else
+    if ( bt_down ) begin
+      q       <= sw_i;
+      counter <= counter + 1;
     end
-    else if (key_i[0]) begin
-            q <= sw_i;
-            counter <= counter +1;
-         end
-end
+  end
 
 assign ledr_o = q;
 
-dec_hex dec0
-( .in(counter[7:4]),
- .OUT(hex0_o));
+dec_hex dec0(
+  .in(counter[7:4]),
+  .out(hex0_o     ));
  
 dec_hex dec1
 ( .in(counter[3:0]),
-  .OUT(hex1_o));
+  .out(hex1_o     ));
  
 endmodule
--- lab2/rtl/dec_hex.v
+++ lab2/rtl/dec_hex.v
@@ -1,26 +1,27 @@
 module dec_hex (
-			input        [3:0]    in,
-			output reg   [6:0]    OUT);
-			
-always @ (*) begin
-case (in)
-		4'h0 : OUT = 7'b1000000;
-		4'h1 : OUT = 7'b1111001;
-		4'h2 : OUT = 7'b0100100;
-		4'h3 : OUT = 7'b0110000;
-		4'h4 : OUT = 7'b0011001;
-		4'h5 : OUT = 7'b0010010;
-		4'h6 : OUT = 7'b0000010;
-		4'h7 : OUT = 7'b1111000;
-		4'h8 : OUT = 7'b0000000;
-		4'h9 : OUT = 7'b0010000;
-		4'ha : OUT = 7'b0001000;
-		4'hb : OUT = 7'b0000011;
-		4'hc : OUT = 7'b1000110;
-		4'hd : OUT = 7'b0100001;
-		4'he : OUT = 7'b0000110;
-		4'hf : OUT = 7'b0001110; 
-             default : OUT = 7'b1111111;
-endcase
+  input        [3:0]    in,
+  output reg   [6:0]    out);
+
+always @( * ) begin
+  case( in )
+    4'h0 : out = 7'b1000000;
+    4'h1 : out = 7'b1111001;
+    4'h2 : out = 7'b0100100;
+    4'h3 : out = 7'b0110000;
+    4'h4 : out = 7'b0011001;
+    4'h5 : out = 7'b0010010;
+    4'h6 : out = 7'b0000010;
+    4'h7 : out = 7'b1111000;
+    4'h8 : out = 7'b0000000;
+    4'h9 : out = 7'b0010000;
+    4'ha : out = 7'b0001000;
+    4'hb : out = 7'b0000011;
+    4'hc : out = 7'b1000110;
+    4'hd : out = 7'b0100001;
+    4'he : out = 7'b0000110;
+    4'hf : out = 7'b0001110;
+  default : out = 7'b1111111;
+  endcase
 end
+
 endmodule

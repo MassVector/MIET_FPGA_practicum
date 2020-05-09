c19a98768058ea47c03f7b62babe4e1f4a5c1c9a
Enot4228 <62872569+Enot4228@users.noreply.github.com>
2020-05-09 11:56:32 +0300

    lab2(rework)

---
+++ lab2/rtl/button_deb.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+
+module button_deb(
+
+  input clk_i,
+  input btn_i,
+  output reg state_o = 0,
+  output ondn_o,
+  output onup_o);
+
+reg sync_0 = 0, sync_1 = 0;
+
+
+always @(posedge clk_i) sync_0 <= btn_i;
+always @(posedge clk_i) sync_1 <= sync_0;
+
+reg [1:0] counter = 2'd0;
+
+wire idle = (state_o == sync_1);
+wire max  = &counter;
+
+always @(posedge clk_i)
+  begin
+    if (idle)
+        counter <= 0;
+    else
+      begin
+        counter <= counter + 1;
+          if (max)
+            state_o <= ~state_o;
+     end
+  end
+
+assign ondn_o = ~idle & max & ~state_o;
+assign onup_o = ~idle & max & state_o;
+
+endmodule
--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -2,36 +2,52 @@
 
 
 module counter(
-  input clk100_i,
-  input rstn_i,
-  input [9:0] sw_i,
-  input [1:0] key_i,
-  output [9:0] ledr_o,
-  output [6:0] hex1_o,
-  output [6:0] hex0_o
+    input clk100_i,
+    input rstn_i,
+    input  [9:0] sw_i,
+    input  [1:0] key_i,
+    output [9:0] ledr_o,
+    output [6:0] hex1_o,
+    output [6:0] hex0_o
 );
 
-reg  [7:0] counter = 8'd0 ;
-reg  [9:0] q = 10'b0;
+reg  [7:0] counter;
+reg  [9:0] q;
+wire [1:0] bt_click;
 
-always @ (posedge clk100_i) begin				
-    if (key_i[1]) begin
-        q = 10'b0;
-        counter = 8'd0;
+button_deb bt0(
+  .btn_i   ( !key_i[0]),
+  .ondn_o  ( bt_click[0]),
+  .clk_i   ( clk100_i)
+); 
+button_deb bt1(
+  .btn_i   (!key_i[1]),
+  .ondn_o  (bt_click[1]),
+  .clk_i   (clk100_i)
+);
+
+always @(posedge clk100_i or posedge bt_click[1])
+  begin
+    if  ( bt_click[1] ) begin
+        q <= 10'b0;
+        counter <= 8'd0;
     end
-    else if (key_i[0]) begin
-            q = sw_i;
-            counter = counter + 1;
-         end
-end
+    else
+      if ( bt_click[0] ) begin
+         q <= sw_i;
+         counter <= counter +1;
+      end
+  end
 
 assign ledr_o = q;
 
-dec_hex dec0
-( .in(counter[7:4]),
-  .out(hex0_o));
-  
-dec_hex dec1
-( .in(counter[3:0]),
-  .out(hex1_o));
+dec_hex dec0(
+  .in (counter[7:4]),
+  .out(hex0_o)
+);
+
+dec_hex dec1(
+  .in (counter[3:0]),
+  .out(hex1_o)
+);
 endmodule

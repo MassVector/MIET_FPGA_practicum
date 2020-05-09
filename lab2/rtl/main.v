d8ac9b425caa355d10f5becb6a4928e448a9d616
MozFox <mozzzfoxxx@gmail.com>
2020-05-09 22:58:23 +0300

    Fixed

--- lab2/rtl/main.v
+++ lab2/rtl/main.v
@@ -21,23 +21,26 @@
 
 
 module main(
-  input        [9:0]  sw_i,
-  input               btn_i,
-  input               clk50_i,
-  input               rstn_i,  
-  output       [9:0]  led_o,
-  output       [6:0]  hex0_o,
-  output       [6:0]  hex1_o
+  input   [9:0]  sw_i,
+  input          btn_i,
+  input          clk50_i,
+  input          arstn_i,  
+  output  [9:0]  led_o,
+  output  [6:0]  hex0_o,
+  output  [6:0]  hex1_o
     );       
 
-reg [2:0] button_syncroniser;
-wire key_was_pressed;
+reg  [2:0] button_syncroniser;
+wire       key_was_pressed;
 
-always @( posedge clk50_i ) 
+always @( posedge clk50_i or negedge arstn_i ) 
   begin
-    button_syncroniser[0] <= btn_i;
-    button_syncroniser[1] <= button_syncroniser[0];
-    button_syncroniser[2] <= button_syncroniser[1];
+    if ( !arstn_i )
+      button_syncroniser    <= 3'b0;
+    else
+      button_syncroniser[0] <= btn_i;
+      button_syncroniser[1] <= button_syncroniser[0];
+      button_syncroniser[2] <= button_syncroniser[1];
   end
   
 assign key_was_pressed = ~button_syncroniser[2] & button_syncroniser[1];
@@ -46,31 +49,31 @@ reg [9:0] q;
 
 assign led_o = q;
 
-always @( posedge clk50_i or negedge rstn_i ) 
+always @( posedge clk50_i or negedge arstn_i ) 
   begin
-    if ( !rstn_i ) 
-        q <= 10'd0;
+    if ( !arstn_i ) 
+      q <= 10'd0;
     else if ( key_was_pressed ) 
-        q <= sw_i;
+      q <= sw_i;
   end
 
 reg [7:0] counter;
-always @( posedge clk50_i or negedge rstn_i ) 
+always @( posedge clk50_i or negedge arstn_i ) 
   begin
-    if ( !rstn_i ) 
-        counter <= 8'd0;
+    if ( !arstn_i ) 
+      counter <= 8'd0;
     else if ( key_was_pressed && ( sw_i > 10'd20 ) ) // для примера событие является на sw_i число больше 20
-        counter = counter + 1;
+      counter = counter + 1;
   end
   
 hex hex0 ( 
-  .in( counter[3:0] ),
-  .out( hex0_o      )
+  .in ( counter[3:0] ),
+  .out( hex0_o       )
   );
   
 hex hex1 ( 
-  .in( counter[7:4] ),
-  .out( hex1_o      )
+  .in ( counter[7:4] ),
+  .out( hex1_o       )
   );  
               
 endmodule
--- lab2/tb/main_tb.v
+++ lab2/tb/main_tb.v
@@ -27,50 +27,50 @@ module main_tb(
 localparam CLK_FREQ_MHZ   = 50;
 localparam CLK_SEMIPERIOD = ( 1000 / CLK_FREQ_MHZ) / 2;     
 
-reg  [9:0]   sw_i;
-wire [9:0]   led_o;
+reg   [9:0]  sw_i;
+wire  [9:0]  led_o;
 reg          btn_i;
-reg          rstn_i;
+reg          arstn_i;
 reg          clk50_i;
-wire [6:0]   hex0_o;
-wire [6:0]   hex1_o;
+wire  [6:0]  hex0_o;
+wire  [6:0]  hex1_o;
 
 main DUT (
  .sw_i       (  sw_i     ),
  .led_o      (  led_o    ),
  .clk50_i    (  clk50_i  ),
  .btn_i      (  btn_i    ),
- .rstn_i     (  rstn_i   ),
+ .arstn_i    (  arstn_i  ),
  .hex0_o     (  hex0_o   ),
  .hex1_o     (  hex1_o   )
 );
 
 initial begin
-    clk50_i = 1'b1;
-    forever begin
-        #CLK_SEMIPERIOD clk50_i=~clk50_i;
-    end
+  clk50_i = 1'b1;
+  forever begin
+    #CLK_SEMIPERIOD clk50_i=~clk50_i;
+  end
 end
 
 initial begin
-    rstn_i = 1'b1;
-    #(4*CLK_SEMIPERIOD);
-    rstn_i = 1'b0;
-    #(4*CLK_SEMIPERIOD);
-    rstn_i = 1'b1;
+  arstn_i = 1'b1;
+  #(4*CLK_SEMIPERIOD);
+  arstn_i = 1'b0;
+  #(4*CLK_SEMIPERIOD);
+  arstn_i = 1'b1;
 end
 
 initial begin
-    sw_i  [9:0] = 10'b0;
-    btn_i       = 1'b0;
-    repeat(40)begin
-        #(CLK_SEMIPERIOD - 1);
-        sw_i[9:0] = $random();
-        #(3*CLK_SEMIPERIOD);
-        btn_i = 1'b1;
-        #(3*CLK_SEMIPERIOD);
-        btn_i = 1'b0;
-    end
+  sw_i  [9:0] = 10'b0;
+  btn_i       = 1'b0;
+  repeat(40)begin
+    #(CLK_SEMIPERIOD - 1);
+    sw_i[9:0] = $random();
+    #(3*CLK_SEMIPERIOD);
+    btn_i = 1'b1;
+    #(3*CLK_SEMIPERIOD);
+    btn_i = 1'b0;
+  end
 end    
     
 endmodule

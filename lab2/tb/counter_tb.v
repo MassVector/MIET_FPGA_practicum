e865a276c24c90388394eeee1c8e1a559b929d77
vetka-koluchaya <62946377+vetka-koluchaya@users.noreply.github.com>
2020-05-11 00:46:52 +0300

    fixing steps

--- lab2/tb/counter_tb.v
+++ lab2/tb/counter_tb.v
@@ -14,31 +14,37 @@ module counter_tb(
     wire [6:0] hex1_o;
     
     counter DUT( 
-       .sw_i     (sw_i),
-       .clk100_i (clk100_i),
-       .key_i    (key_i),
-       .ledr_o   (ledr_o),
-       .hex0_o   (hex0_o),
-       .hex1_o   (hex1_o)
+       .sw_i   (sw_i),
+       .clk_i  (clk100_i),
+       .key_i  (key_i),
+       .ledr_o (ledr_o),
+       .hex0_o (hex0_o),
+       .hex1_o (hex1_o)
     );
     
-    assign key_i[1] = 0;
-    assign key_i[0] = 1;
-    
-    //assign key_i[1]=1;
-    //assign key_i[0]=0;
-    
+   reg [1:0] sw = 2'b11;
+     
     initial begin
        clk100_i = 1'b1;
        forever begin
-       #CLK_SEMIPERIOD clk100_i=~clk100_i;
+          #CLK_SEMIPERIOD clk100_i=~clk100_i;
        end
     end
     
     initial begin
-       sw_i[9:0] = 10'd0;
-       repeat (32) begin
-       #(CLK_SEMIPERIOD-1) sw_i[9:0]=$random();
+       sw_i[9:0] = 10'd1;
+       forever begin
+          #(CLK_SEMIPERIOD-1); 
+          sw_i[9:0] = $random();
        end
     end
+    
+    initial begin
+       repeat (16) begin
+       #50;
+       sw = $random();
+       end
+    end
+    assign key_i = sw;
+    
 endmodule

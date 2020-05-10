4b570e4028874d1c49bc42959a1c715f36cefef9
logal24 <https://github.com/logal24/MIET_FPGA_practicum.git>
2020-05-10 19:49:48 +0300

    change1

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -1,35 +1,32 @@
 `timescale 1ns / 1ps
-
-
-
 module counter(
+  input        clk100_i,
+  input        rstn_i,
+  input  [9:0] sw_i,
+  input  [1:0] key_i,
+  output [9:0] ledr_o,
+  output [6:0] hex1_o,
+  output [6:0] hex0_o
+);
+reg [9:0] ledr    = 10'b0;
+reg [7:0] counter =  8'd0;
+always @( posedge clk100_i or negedge key_i[1] ) begin
+  if ( key_i[1] ) begin
+    ledr    <=  10'b0;
+    counter <=  8'd0;
+  end  
+  else
+   if ( key_i[0] ) begin
+     ledr    <= sw_i;
+     counter <= counter + 1;
+   end 
+end
 
-  input      [3:0] in,
-  output reg [6:0] out
-  
-  );
-  
-  always @(*) begin
-  case(in)
-     4'h0 : out = 7'b1000000;
-     4'h1 : out = 7'b1111001;
-     4'h2 : out = 7'b0100100;
-     4'h3 : out = 7'b0110000;
-     4'h4 : out = 7'b0011001;
-     4'h5 : out = 7'b0010010;
-     4'h6 : out = 7'b0000010;
-     4'h7 : out = 7'b1111000;
-     4'h8 : out = 7'b0000000;
-     4'h9 : out = 7'b0010000;
-     4'ha : out = 7'b0001000;
-     4'hb : out = 7'b0000011;
-     4'hc : out = 7'b1000110;
-     4'hd : out = 7'b0100001;
-     4'he : out = 7'b0000110;
-     4'hf : out = 7'b0001110;
-   default : out = 7'b1111111;
-  endcase
- end 
-    
-      
+assign ledr_o = counter;
+hex dec0
+( .in ( counter[7:4] ),
+  .out ( hex0_o ) );
+hex dec1
+( .in ( counter[3:0] ),
+  .out ( hex1_o ) );
 endmodule

32ffb9d3af8d451987ed281f55af43aa17477baf
krsnvir <irinlolirina@gmail.com>
2020-05-11 15:20:35 +0500

    Fixed bugs with plug-in ports. Fixed hex decoder.

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -12,15 +12,14 @@ module counter#(
   output [6:0] hex0_o
   );
   
-  reg       sw_event;
-  wire      key;
-  reg [7:0] counter_i;
-  
+  reg        sw_event;
+  reg  [7:0] counter_i;
+
   always @( posedge clk100_i or negedge rstn_i ) begin
    if ( !rstn_i )
      counter_i <= {DATA_WIDTH{1'b0}};
    else 
-       if( key && sw_event ) 
+       if( key_i[0] && sw_event ) 
          counter_i <= counter_i + 1;
    end
    
@@ -32,19 +31,17 @@ module counter#(
   REG_TEN reg_ten(
   .clk100_i   ( clk100_i       ),
   .rstn_i     ( key_i    [1]   ),
-  .sw_i       ( sw_i     [9:0] ),
+  .sw_i       ( sw_i           ),
   .key_i      ( key_i    [0]   ),
-  .led_o      ( ledr_o   [9:0] )
+  .led_o      ( ledr_o         )
   );
   
   decoder_hex decoder_0(
-  .arstn_i    ( key_i       [1]   ),
   .kod_i      ( counter_i   [3:0] ),
   .hex_o      ( hex0_o      [6:0] )
   );  
   
   decoder_hex decoder_1(
-  .arstn_i( key_i           [1]   ),
   .kod_i  ( counter_i       [7:4] ),
   .hex_o  ( hex1_o          [6:0] )
   );  

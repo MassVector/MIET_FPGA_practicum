0723a290efdb65a9b48a2e3e7f76999db2487686
M4RKERGit <62885141+M4RKERGit@users.noreply.github.com>
2020-05-11 19:22:41 +0300

    Add files via upload

--- lab2/rtl/button_fix.v
+++ lab2/rtl/button_fix.v
@@ -2,26 +2,21 @@
 
 
 module button_fix(
-    input clk100_i,
-    input  reset_i,
-    input key_i,
-    output ondn_o);
+  input  clk100_i,
+  input  reset_i,
+  input  key_i,
+  output ondn_o);
 
-reg sync[1:0];
-
-reg counter;
+reg [1:0] sync;
 
 always @( posedge clk100_i or negedge reset_i ) begin
-if( !reset_i )
-begin
-  sync[0] <= 2'b0;
-  sync[1] <= 2'b0;
-end
+  if( !reset_i )
+    sync <= 2'b0;
   else
-  begin
-  sync[0] <= key_i;
-  sync[1] <= sync[0];
-  end
+    begin
+      sync[0] <= key_i;
+      sync[1] <= sync[0];
+    end
 end
 
 assign ondn_o = ~sync[1] & sync[0];

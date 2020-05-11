70a54d4f5bc54d31f0ce3407a0d0bef91750782a
vetka-koluchaya <62946377+vetka-koluchaya@users.noreply.github.com>
2020-05-11 17:08:55 +0300

    Change reg name & check code

--- lab2/tb/counter_tb.v
+++ lab2/tb/counter_tb.v
@@ -14,12 +14,12 @@ module counter_tb(
     wire [6:0] hex1_o;
     
     counter DUT( 
-       .sw_i   (sw_i),
-       .clk_i  (clk100_i),
-       .key_i  (key_i),
-       .ledr_o (ledr_o),
-       .hex0_o (hex0_o),
-       .hex1_o (hex1_o)
+       .sw_i      (sw_i),
+       .clk100_i  (clk100_i),
+       .key_i     (key_i),
+       .ledr_o    (ledr_o),
+       .hex0_o    (hex0_o),
+       .hex1_o    (hex1_o)
     );
     
    reg [1:0] sw = 2'b11;
@@ -34,7 +34,7 @@ module counter_tb(
     initial begin
        sw_i[9:0] = 10'd1;
        forever begin
-          #(CLK_SEMIPERIOD-1); 
+          #(CLK_SEMIPERIOD - 1); 
           sw_i[9:0] = $random();
        end
     end

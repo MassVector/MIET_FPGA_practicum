f059b1076949f324b1a59c11d66203c2f94b8dd8
Kirill <hufsamarran@gmail.com>
2020-05-11 20:17:32 +0300

    Added debounce module and fixed codestyle problems

---
+++ lab2/rtl/debouncer.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+
+
+module debouncer(
+  input en_i,
+  input rstn_i,
+  input clk_i,
+  
+  output synced_signal_o
+  );
+    
+reg [1:0] button_syncroniser;
+
+always @( posedge clk_i or negedge rstn_i ) begin
+  if( !rstn_i )
+    button_syncroniser <= 2'b0;
+  else begin
+    button_syncroniser[0] <= ~en_i;
+    button_syncroniser[1] <= button_syncroniser[0];
+  end
+end
+
+assign synced_signal_o = ~button_syncroniser[1] & button_syncroniser[0];
+
+endmodule
--- lab2/tb/counter_tb.v
+++ lab2/tb/counter_tb.v
@@ -5,18 +5,20 @@ module counter_tb();
 
 reg  [9:0]  sw_i;
 reg  [1:0]  key_i;
-reg        clk100_i;
+reg         rstn_i;
+reg         clk100_i;
 wire [9:0]  ledr_o;
 wire [6:0]  hex0_o;
 wire [6:0]  hex1_o;
 
 counter DUT(
-  .sw_i(     sw_i     ),
-  .key_i(    key_i    ),
-  .clk100_i( clk100_i ),
-  .ledr_o(   ledr_o   ),
-  .hex0_o(   hex0_o   ),
-  .hex1_o(   hex1_o   )
+  .sw_i     ( sw_i     ),
+  .rstn_i   ( rstn_i   ),
+  .key_i    ( key_i    ),
+  .clk100_i ( clk100_i ),
+  .ledr_o   ( ledr_o   ),
+  .hex0_o   ( hex0_o   ),
+  .hex1_o   ( hex1_o   )
 ); 
 
 initial begin 

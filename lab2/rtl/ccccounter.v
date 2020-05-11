85c2f89365060fe0cad79ecacf4169e0527311d6
M4RKERGit <62885141+M4RKERGit@users.noreply.github.com>
2020-05-11 19:25:53 +0300

    Add files via upload

--- lab2/rtl/ccccounter.v
+++ lab2/rtl/ccccounter.v
@@ -1,49 +1,30 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.05.2020 15:12:05
-// Design Name: 
-// Module Name: ccccounter
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module ccccounter(
-    input        clk100_i,
-    input        rstn_i,
-    input  [1:0] key_i,
-    output [6:0] hex1_o,
-    output [6:0] hex0_o
-    );
-    reg  [7:0] counter = 8'd0 ;
+  input        clk100_i,
+  input        rst_i,
+  input        bt_down,
+  output [6:0] hex1_o,
+  output [6:0] hex0_o);
 
-always @ (posedge clk100_i) begin				
-    if  ( key_i[1] ) begin //тут прописываем сброс в дефолт
-        counter = 8'd0;
+reg  [7:0] counter;
+
+always @( posedge clk100_i or negedge rst_i ) begin
+  if( !rst_i ) begin //тут прописываем сброс в дефолт
+    counter <= 8'd0;
+  end
+  else
+    if( bt_down ) begin //тут увеличиваем counter
+      counter <= counter +1;
     end
-    else if ( key_i[0] ) begin //тут увеличиваем counter
-            counter = counter +1;
-         end
 end
 
 dec_hex dec0
-( .in(counter[7:4]),
- .OUT(hex0_o));
- 
+(.in  ( counter[7:4] ),
+ .out ( hex0_o       ));
+
 dec_hex dec1
-( .in(counter[3:0]),
-  .OUT(hex1_o));
-  
+(.in  ( counter[3:0] ),
+ .out ( hex1_o       ));
+
 endmodule
--- lab2/rtl/reg_to_ledr.v
+++ lab2/rtl/reg_to_ledr.v
@@ -1,41 +1,22 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.05.2020 14:02:46
-// Design Name: 
-// Module Name: reg_to_ledr
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module reg_to_ledr(
-    input        clk100_i,
-    input        rstn_i,
-    input  [9:0] sw_i,
-    input  [1:0] key_i,
-    output [9:0] ledr_o
-    );
-reg  [9:0] q = 10'b0;
+  input        clk100_i,
+  input        rst_i,
+  input        bt_down,
+  input  [9:0] sw_i,
+  output [9:0] ledr_o);
 
-always @ (posedge clk100_i) begin				
-    if  ( key_i[1] ) begin //тут прописываем сброс в дефолт
-        q = 10'b0;
+reg  [9:0] q;
+
+always @( posedge clk100_i or negedge rst_i ) begin				
+  if( !rst_i ) begin //тут прописываем сброс в дефолт
+    q <= 10'b0;
+  end
+  else 
+    if( bt_down ) begin //тут записываем данные с SW
+      q <= sw_i;
     end
-    else if ( key_i[0] ) begin //тут записываем данные с SW
-            q = sw_i;
-         end
 end
 
 assign ledr_o = q;

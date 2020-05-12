17c0865dddb4a9e60b4da6148e48511664f558ea
Aziz Lipkoffsky <azizshokiri@gmail.com>
2020-05-12 16:25:26 +0300

    Delete unused port sw_mode_i

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -5,7 +5,6 @@ module counter(
   input         clk100_i,
   input   [9:0] sw_i,
   input   [1:0] key_i,
-  input         sw_mode_i,
   
   output  [9:0] ledr_o,
   output  [6:0] hex1_o,
@@ -15,7 +14,7 @@ module counter(
 );
   
   reg  [7:0] counter;
-  reg        sw_mode;
+
   wire       key_pressed;
   wire       key_released;
   

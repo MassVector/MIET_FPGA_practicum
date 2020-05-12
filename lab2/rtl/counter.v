21e9d20abb9fba3c7a1138f42c2ba5767b7b4b69
Aziz Lipkoffsky <azizshokiri@gmail.com>
2020-05-12 16:13:27 +0300

    Добавлено ограничение счетчика

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -5,6 +5,7 @@ module counter(
   input         clk100_i,
   input   [9:0] sw_i,
   input   [1:0] key_i,
+  input         sw_mode_i,
   
   output  [9:0] ledr_o,
   output  [6:0] hex1_o,
@@ -12,18 +13,28 @@ module counter(
   
   
 );
-
+  
   reg  [7:0] counter;
-  
-  
+  reg        sw_mode;
   wire       key_pressed;
+  wire       key_released;
   
+  reg        out_of_range;
+  wire       signal;
+  
+  assign signal = key_released;
   
   always @( posedge clk100_i or negedge key_i[1] ) begin
-    if( !key_i[1] )
+    if( !key_i[1] ) begin
       counter <= 0;
-    else if( key_pressed ) begin
-      counter <= counter + 1;
+      out_of_range <=0;
+    end else if( signal ) begin
+      if ( counter > 'd99) out_of_range <= 1'b1;
+      else  counter <= counter + 1;
+      if ( out_of_range ) begin
+        counter[3:0] = 'd14;
+        counter[7:4] = 'd14;
+      end   
     end
   end
   
@@ -34,14 +45,14 @@ debounce d(
   .rst_i        ( !key_i   [1] ),
   .en_i         ( key_i    [0] ),
   .en_down_o    ( key_pressed  ), 
-  .en_up_o      (              )
+  .en_up_o      ( key_released )
 );
   
   
 register r(
   .clk_i        ( clk100_i     ),
-  .rstn_i       ( key_i[1]     ),
-  .en_i         ( key_pressed ),
+  .rstn_i       ( !key_i[1]    ),
+  .en_i         ( signal       ),
   .data_i       ( sw_i         ),
 
   .data_o       ( ledr_o       )

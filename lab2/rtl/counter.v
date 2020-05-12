7d1e24f927c73b04875462bf6a263c3d07d7e0af
gforce0617 <62894421+gforce0617@users.noreply.github.com>
2020-05-12 21:06:41 +0400

    Revert "1"
    This reverts commit 608ab89dd5391f8b417ed43f501da004c331026d.

---
+++ lab2/rtl/counter.v
@@ -0,0 +1,13 @@
+module counter(
+    input clk100_i,
+    input rstn_i,
+    input [9:0] sw_i,
+    input [1:0] key_i,
+    output [9:0] ledr_o,
+    output [6:0] hex1_o,
+    output [6:0] hex0_o
+);
+
+
+
+endmodule

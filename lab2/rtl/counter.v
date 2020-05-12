59076287386109af9d5a086843ca407a3d6e8ecf
capython59 <62896832+capython59@users.noreply.github.com>
2020-05-12 20:19:23 +0300

    Update counter.v

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -1,23 +1,22 @@
 `timescale 1ns / 1ps
 
 module counter(
-  input        clk100_i,
-   
-  input  [9:0] sw_i,
-  input  [1:0] key_i,
+  input         clk100_i,
+  input  [1:0]  key_i, 
+  
+  input  [9:0]  sw_i, 
+  output [9:0]  ledr_o,
     
-  output [9:0] ledr_o,
-    
-  output [6:0] hex0_o,
-  output [6:0] hex1_o,
-  output [6:0] hex2_o,
-  output [6:0] hex3_o
+  output [6:0]  hex0_o,
+  output [6:0]  hex1_o,
+  output [6:0]  hex2_o,
+  output [6:0]  hex3_o
 );
 
-wire  [6:0]     hex0_d;
-wire  [6:0]     hex1_d;
-wire  [6:0]     hex2_d;
-wire  [6:0]     hex3_d;
+wire  [6:0]      hex0_d;
+wire  [6:0]      hex1_d;
+wire  [6:0]      hex2_d;
+wire  [6:0]      hex3_d;
 
 assign  hex0_o = hex0_d;
 assign  hex1_o = hex1_d;

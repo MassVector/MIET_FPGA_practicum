aa77dee2931e499175abba80cfe57d3f10ef672d
Niheim <dark.lopyrev@gmail.com>
2020-05-13 19:24:04 +0300

    lab2_ind_task_5+6

--- lab2/tb/counter_tb.v
+++ lab2/tb/counter_tb.v
@@ -8,12 +8,12 @@ module counter_tb(
   localparam CLK_FREQ_MHZ = 50;
   localparam CLK_SEMIPERIOD = (1000 / CLK_FREQ_MHZ)/2;
  
-  reg        clk100_i;
-  reg  [9:0] sw_i;
-  reg  [1:0] key_i;
-  wire [9:0] ledr_o;
-  wire [6:0] hex0_o;
-  wire [6:0] hex1_o;
+  reg         clk100_i;
+  reg  [11:0] sw_i;
+  reg  [1:0]  key_i;
+  wire [9:0]  ledr_o;
+  wire [6:0]  hex0_o;
+  wire [6:0]  hex1_o;
   
   
    counter DUT
@@ -33,17 +33,17 @@ module counter_tb(
    end
    
    initial begin
-     sw_i[9:0] = 10'b0;
+     sw_i[11] = 1'b1;
+     sw_i[10:0] = 11'b0;
      key_i[1] = 1'b0;
      key_i[0] = 1'b1;
-     #70 key_i[1] = 1'b1;
-     #50 key_i[0] = 1'b0;
+     #30 key_i[1] = 1'b1;
    end
    
    
    initial begin
      forever begin
-       #CLK_SEMIPERIOD sw_i = $random();
+       #CLK_SEMIPERIOD sw_i [9:0] = $random();
        #(1.6*CLK_SEMIPERIOD) key_i[0] = ~key_i[0];
      end
    end

b381ffd4eeb9eeaa5f74c4e0a7debf8ef436d634
vetka-koluchaya <62946377+vetka-koluchaya@users.noreply.github.com>
2020-05-13 22:36:05 +0300

    Individual task 6+7

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -2,6 +2,7 @@
 
 module counter(
   input        clk100_i,
+  input        rstn_i,
   input  [9:0] sw_i,
   input  [1:0] key_i,
   output [9:0] ledr_o,
@@ -9,9 +10,7 @@ module counter(
   output [6:0] hex0_o
   );
     
-  reg [9:0] register;
-  reg [7:0] cntr;
-  
+  reg [7:0] cntr = 8'd0;  
   wire bt_down;
   
   pressed btdown(
@@ -20,29 +19,32 @@ module counter(
     .clk100_i (clk100_i),
     .btnd_o   (bt_down)
     );
-  
-  always @(posedge clk100_i or negedge key_i[1]) begin
-         
+
+  registerrrrr reg_01( 
+    .in        (key_i [0]),
+    .rstn_i    (key_i [1]),
+    .clk100_i  (clk100_i),
+    .data_i    (sw_i   [9:0]),
+    .data_o    (ledr_o [9:0])
+); 
+ 
+  always @(posedge clk100_i or negedge key_i[1]) begin         
     if (!key_i[1]) begin
-        register <= 10'b0;
-        cntr <= 8'd0;
+         cntr <= 8'd55;
     end
     else
       if (bt_down) begin
-        register <= sw_i;
-        cntr <= cntr + 1;
+         cntr <= cntr + 1;
     end
   end
-    
-  assign ledr_o = register;
   
   dectohex dec0
-  ( .dec(cntr[3:0]),
+    ( .dec(cntr[7:4]),
       .hex(hex0_o)
     );
     
   dectohex dec1
-  ( .dec(cntr[7:4]),
+    ( .dec(cntr[3:0]),
       .hex(hex1_o)
     );
     

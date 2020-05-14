5932fd043a0830087bbd42b022d19abad99eb688
M4RKERGit <62885141+M4RKERGit@users.noreply.github.com>
2020-05-14 14:31:26 +0300

    Add files via upload

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -9,41 +9,30 @@ module counter(
   output [6:0]  hex1_o,
   output [6:0]  hex0_o);
 
-wire bt_down;
-reg [1:0] indc;
-reg [7:0] ind_out;
-reg [7:0] cnt1;
-reg [7:0] cnt2;
+wire       bt_down;
+reg  [1:0] indc;
+wire [7:0] ind_out;
+reg  [7:0] cntdown;
+reg  [7:0] cntup;
 
 always @( posedge clk100_i or negedge key_i[1] ) begin
   if( !key_i[1] ) begin
-  cnt1 <= 8'h0;
-  cnt2 <= 8'h0;
+  cntdown <= 8'h0;
+  cntup <= 8'h0;
   indc <= 8'b0;
   end
   else begin
     if( bt_down )
     indc <= indc + 1;
   end
+    if ( indc==3 ) begin
+      indc <= 0;
+      cntup  <= cntup + 1;  
+      cntdown  <= cntdown - 1;
+  end
 end
 
-always @( * ) begin
-  if ( indc==3 ) begin
-    indc <= 0;
-    if( cnt2 < 8'hFF )
-      cnt2  <= cnt2 + 1;
-    else
-      cnt2  <= ~cnt2;
-    if( cnt1 != 8'h0 )
-      cnt1  <= cnt1 - 1;
-    else
-      cnt1  <= ~cnt1;
-  end
-  if( !sw_i[11] )
-    ind_out <= cnt1;
-  else
-    ind_out <= cnt2;
-end
+assign ind_out = ( sw_i[11] ) ? ( cntdown ) : ( cntup );
 
 reg_to_ledr secmod
 ( .sw_i       ( sw_i      ),

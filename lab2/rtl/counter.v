6978935959150ab2d490830efc72e69c1a32d9dc
Niheim <dark.lopyrev@gmail.com>
2020-05-14 20:52:11 +0300

    lab2_ind_fix

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -1,3 +1,5 @@
+`timescale 1ns / 1ps
+ 
   module counter(
 
     input             clk100_i,
@@ -12,6 +14,7 @@
   reg [9:0] rg10;
   reg [7:0] counter_p;
   reg [7:0] counter_m;
+  reg [7:0] counter;
   
   
   reg [2:0] btn_sync;
@@ -54,51 +57,16 @@
       counter_m <= counter_m - 1;
     end
   end
- 
   
   always @ ( * ) begin
-    if ( sw_i[11] == 0 ) begin
-      case ( counter_p [3:0] )
-        4'b0000 : hex0_o = 7'b100_0000;
-        4'b0001 : hex0_o = 7'b111_1001;
-        4'b0010 : hex0_o = 7'b010_0100;
-        4'b0011 : hex0_o = 7'b011_0000;
-        4'b0100 : hex0_o = 7'b001_1001;
-        4'b0101 : hex0_o = 7'b001_0010;
-        4'b0110 : hex0_o = 7'b000_0010;
-        4'b0111 : hex0_o = 7'b111_1000;
-        4'b1000 : hex0_o = 7'b000_0000;
-        4'b1001 : hex0_o = 7'b001_0000;
-        4'b1010 : hex0_o = 7'b000_1000;
-        4'b1011 : hex0_o = 7'b000_0011;
-        4'b1100 : hex0_o = 7'b100_0110;
-        4'b1101 : hex0_o = 7'b010_0001;
-        4'b1110 : hex0_o = 7'b000_0110;
-        4'b1111 : hex0_o = 7'b000_1110;
-      endcase
-      
-      case ( counter_p [7:4] )
-        4'b0000 : hex1_o = 7'b100_0000;
-        4'b0001 : hex1_o = 7'b111_1001;
-        4'b0010 : hex1_o = 7'b010_0100;
-        4'b0011 : hex1_o = 7'b011_0000;
-        4'b0100 : hex1_o = 7'b001_1001;
-        4'b0101 : hex1_o = 7'b001_0010;
-        4'b0110 : hex1_o = 7'b000_0010;
-        4'b0111 : hex1_o = 7'b111_1000;
-        4'b1000 : hex1_o = 7'b000_0000;
-        4'b1001 : hex1_o = 7'b001_0000;
-        4'b1010 : hex1_o = 7'b000_1000;
-        4'b1011 : hex1_o = 7'b000_0011;
-        4'b1100 : hex1_o = 7'b100_0110;
-        4'b1101 : hex1_o = 7'b010_0001;
-        4'b1110 : hex1_o = 7'b000_0110;
-        4'b1111 : hex1_o = 7'b000_1110;  
-      endcase
-    end
+    if ( sw_i[11] == 0 )
+      counter <= counter_p;
     else
-    if ( sw_i[11] == 1 ) begin
-      case ( counter_m [3:0] )
+      counter <= counter_m;
+  end
+  
+  always @ ( * ) begin
+      case ( counter [3:0] )
         4'b0000 : hex0_o = 7'b100_0000;
         4'b0001 : hex0_o = 7'b111_1001;
         4'b0010 : hex0_o = 7'b010_0100;
@@ -117,7 +85,7 @@
         4'b1111 : hex0_o = 7'b000_1110;
       endcase
       
-      case ( counter_m [7:4] )
+      case ( counter [7:4] )
         4'b0000 : hex1_o = 7'b100_0000;
         4'b0001 : hex1_o = 7'b111_1001;
         4'b0010 : hex1_o = 7'b010_0100;
@@ -135,7 +103,6 @@
         4'b1110 : hex1_o = 7'b000_0110;
         4'b1111 : hex1_o = 7'b000_1110;  
       endcase
-    end
   end
 
 endmodule

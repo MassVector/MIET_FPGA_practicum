1f965657903c24417fd9df228f11fe64bc0df8e7
M4RKERGit <62885141+M4RKERGit@users.noreply.github.com>
2020-05-16 17:45:14 +0300

    Add files via upload

--- lab2/rtl/counter.v
+++ lab2/rtl/counter.v
@@ -10,7 +10,7 @@ module counter(
   output [6:0]  hex0_o);
 
 wire       bt_down;
-reg  [1:0] indc;
+reg  [4:0] indc;
 wire [7:0] ind_out;
 reg  [7:0] cntdown;
 reg  [7:0] cntup;
@@ -20,20 +20,22 @@ always @( posedge clk100_i or negedge key_i[1] ) begin
   if( !key_i[1] ) begin
     cntdown <= 8'h0;
     cntup   <= 8'h0;
-    indc    <= 8'b0;
+    indc    <= 4'b0;
     q       <= 10'b0;
   end
-  else begin
-    if( bt_down ) begin
-      indc <= indc + 1;
-      if( indc == 3 ) begin
-        indc    <= 0;
-        cntup   <= cntup + 1;
-        cntdown <= cntdown - 1;
-        q       <= sw_i[9:0];
+  else
+    begin
+      if( bt_down ) begin
+        if( ( indc + 1 ) == 3 ) begin
+          indc    <= 0;
+          cntup   <= cntup + 1;
+          cntdown <= cntdown - 1;
+          q       <= sw_i[9:0];
+        end
+          else
+            indc <= indc + 1;
       end
     end
-  end
 end
 
 assign ind_out = ( sw_i[11] ) ? ( cntdown ) : ( cntup );

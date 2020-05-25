228d91f0385568a2b89a9746cc4a3a4fee25cb5e
MassVector <40513739+MassVector@users.noreply.github.com>
2020-05-25 21:00:35 +0300

    Muravyov_lab3
    done

--- lab3/rtl/stopwatch.v
+++ lab3/rtl/stopwatch.v
@@ -34,16 +34,15 @@ always @( posedge clk100_i )
 
 always @( posedge clk100_i )
   begin
-    start_stop_pressed ? device_state = ~device_state;
+    start_stop_pressed & (( state == STATE_DEFAULT ) | (state == STOPWATCH_MODE )) ? device_state = ~device_state;
 
-    device_state == 1 ? pulse_counter <= pulse_counter + 1;
+    device_state ? pulse_counter <= pulse_counter + 1;
 
-    device_state == 0 | pulse_counter_passed ? pulse_counter <= 0;
-  end
+    !device_state | pulse_counter_passed ? pulse_counter <= 0;
 
 always @( posedge rstn_i )
   begin
-    if ( rstn_i == 0) begin
+    if ( !rstn_i ) begin
       pulse_counter <= 0;
       device_state <= 0;
       number_00_01 <= 0;
@@ -124,5 +123,101 @@ hex hex0
     .hex_i ( number_00_01 ),
     .hex_o ( hex0_o       )
   );
+
+
+reg [2:0] state;
+reg [2:0] next_state;
+reg [2:0] set_syncroniser;
+reg [2:0] change_syncroniser;
+
+localparam STATE_DEFAULT  = 3'd0;
+localparam STATE_1        = 3'd1;
+localparam STATE_2        = 3'd2;
+localparam STATE_3        = 3'd3;
+localparam STATE_4        = 3'd4;
+localparam STOPWATCH_MODE = 3'd5;
+
+wire set_pressed;
+wire change_pressed;
+
+assign set_pressed    = ~set_syncroniser[2] & set_syncroniser[1];
+assign change_pressed = ~change_syncroniser[2] & change_syncroniser[1];
+
+always @( posedge clk100_i ) 
+  begin
+    set_syncroniser[0] <= !set_i;
+    set_syncroniser[1] <= set_syncroniser[0];
+    set_syncroniser[2] <= set_syncroniser[1];
+  end
+  
+always @( posedge clk100_i )  
+  begin
+    change_syncroniser[0] <= !change_i;
+    change_syncroniser[1] <= change_syncroniser[0];
+    change_syncroniser[2] <= change_syncroniser[1];
+
+  end
+
+always @( posedge clk100 ) begin
+  if ( set_pressed & device_state ) begin
+    pulse_counter <= 0;
+    device_state <= 0;
+    number_00_01 <= 0;
+    number_00_10 <= 0;
+    number_01_00 <= 0;
+    number_10_00 <= 0;
+  end    
+end
+
+always @( posedge clk100_i or posedge rstn_i ) begin
+  state <= next_state;
+end
+
+always @( * ) begin 
+  case ( state )
+    STATE_DEFAULT : if ( set_pressed ) next_state <= STATE_1;
+                    else if ( start_stop_pressed ) begin
+                      next_state <= STOPWATCH_MODE;
+                    end
+                    else next_state <= STATE_DEFAULT;
+
+    STATE_1 :       if ( set_pressed ) next_state <= STATE_2;
+                    else if ( change_pressed ) begin
+                      if ( number_00_01 != 4'd9 ) number_00_01 = number_00_01 + 1;
+                      else number_00_01 = 0;
+                      next_state <= STATE_1;
+                    end
+                    else next_state <= STATE_1;
+    
+    STATE_2 :       if ( set_pressed ) next_state <= STATE_3;
+                    else if ( change_pressed ) begin
+                      if ( number_00_10 != 4'd9 ) number_00_10 = number_00_10 + 1;
+                      else number_00_10 = 0;
+                      next_state <= STATE_2;
+                    end
+                    else next_state <= STATE_2;
+    
+    STATE_3 :       if ( set_pressed ) next_state <= STATE_4;
+                    else if ( change_pressed ) begin
+                      if ( number_01_00 != 4'd9 ) number_01_00 = number_01_00 + 1;
+                      else number_01_00 = 0;
+                      next_state <= STATE_3;
+                    end
+                    else next_state <= STATE_3;
+          
+    STATE_4 :       if ( set_pressed ) next_state <= STATE_DEFAULT;
+                    else if ( change_pressed ) begin
+                      if ( number_10_00 != 4'd9 ) number_10_00 = number_10_00 + 1;
+                      else number_10_00 = 0;
+                      next_state <= STATE_4;
+                    end
+                    else next_state <= STATE_4;
+              
+    STOPWATCH_MODE : if ( start_stop_pressed ) next_state <= STATE_DEFAULT;
+                     else next_state <= STOPWATCH_MODE;
+                 
+    default : next_state <= STATE_DEFAULT;
+  endcase
+end
 
 endmodule
